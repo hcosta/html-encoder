2a302b2d390b7ad414b112cf992d28a6124df859
Héctor Costa Guzmán <hcostaguzman@gmil.com>
2014-02-13 12:58:09 +0100

    updating critical error

--- js/parser.js
+++ js/parser.js
@@ -2,7 +2,7 @@ function htmlEscape(str) {
     return String(str)
             .replace(/&/g, '&amp;')
             .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#apos;')
+            .replace(/'/g, '&apos;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
             .replace(/{/g, '&#123;')
